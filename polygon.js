6484de1d13e883e92d32808e31279c72e0f071d9
kanishk-navaneethan <77451873+kanishk-navaneethan@users.noreply.github.com>
2021-05-17 19:11:32 +0530

    Update polygon.js

--- polygon.js
+++ polygon.js
@@ -1,7 +1,7 @@
 class Polygon extends BaseClass {
     constructor(x,y){
       super(x,y,50,50);
-      this.image = loadImage("sprites/polygon.png");
+      this.image = loadImage("polygon.png");
     }
   
     display() {
